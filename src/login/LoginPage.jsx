999abd8e8831b61d572c46bcc231553e4d4d10fe
seungyun-Park <tmdrbs0925@gmail.com>
2024-11-28 00:29:19 +0900

    kakaoID

--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -60,7 +60,9 @@ export default function LoginPage() {
         console.log("Base URL:", baseUrl);
         console.log("Kakao Client ID:", process.env.REACT_APP_KAKAO_CLIENT_ID);
         window.open(
-            `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${baseUrl}/auth/callback/kakao&response_type=code`,
+            `https://kauth.kakao.com/oauth/authorize?client_id=${
+                "fc36040f829290b13c8b48dcbc521c33" /*process.env.REACT_APP_KAKAO_CLIENT_ID*/
+            }&redirect_uri=${baseUrl}/auth/callback/kakao&response_type=code`,
             "_self"
         );
     };
