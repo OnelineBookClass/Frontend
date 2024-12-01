80f3fc556dabe8e711915367c12f5819d4230bd6
seungyun-Park <tmdrbs0925@gmail.com>
2024-12-02 02:31:05 +0900

    [FIX] 요청 주소 서버 배포주소로 수정

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://mongdangbul.store/"
 });
 
 axiosInstance.interceptors.request.use(
